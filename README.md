ec54a0108c828e0cc7a328b3cbe96a558f4a386c
kingvkumar <king.vkumar@gmail.com>
2019-12-08 02:45:34 +0530

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# JS.React
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="content">
+      <div className="header">Header</div>
+      <div className="main">Main</div>
+      <div className="footer">Footer</div>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
